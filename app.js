0ec037b6a94859a1da1a08930c86a37a1b464f7a
Mehul Chouradia <mehulchouradiamc@gmail.com>
2022-07-06 10:51:14 +0530

    update

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 console.log("How areyou doing ?");
 
-var translateBtn=document.querySelector("#btn");
+var translateBtn = document.querySelector("#btn");
 
+var txtInput=document.querySelector("#txt-input");
+var ip=txtInput.innerText("Jai Shree Ram");
 
-function  clickHandler(){
-    console.log("Clicked");
+function clickHandler() {
+    console.log(ip);
 }
 
-translateBtn.addEventListener("click",clickHandler);
+translateBtn.addEventListener("click", clickHandler);
