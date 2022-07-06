490b73005f3bd9933c443798f081ae42e6311f61
Mehul Chouradia <mehulchouradiamc@gmail.com>
2022-07-06 07:05:39 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("How areyou doing ?");
+
+var translateBtn=document.querySelector("#btn");
+
+
+function  clickHandler(){
+    console.log("Clicked");
+}
+
+translateBtn.addEventListener("click",clickHandler);
