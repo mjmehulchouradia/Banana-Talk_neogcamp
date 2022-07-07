354e0819eec28d28930dfe90e74735c90f6eabd9
Mehul Chouradia <mehulchouradiamc@gmail.com>
2022-07-07 06:52:57 +0530

    Input Output processing

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ console.log("How areyou doing ?");
 var translateBtn = document.querySelector("#btn");
 
 var txtInput=document.querySelector("#txt-input");
-var ip=txtInput.innerText("Jai Shree Ram");
+var divOutput=document.querySelector("#output");
+
+// divOutput.innerText="Mehul Jain"
+
+// console.log(divOutput);
 
 function clickHandler() {
-    console.log(ip);
+    console.log("Clicked");
+    var txt=txtInput.value;
+    divOutput.innerText=txt;
+    console.log(divOutput);
 }
 
 translateBtn.addEventListener("click", clickHandler);
