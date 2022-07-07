13e377c97e76b4a0509a4b1f947475b18d3e234c
Mehul Chouradia <mehulchouradiamc@gmail.com>
2022-07-07 08:25:25 +0530

    Banana talk code
    Banana talk basic code completed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("How areyou doing ?");
+console.log("How are you doing ?");
 
 var translateBtn = document.querySelector("#btn");
 
@@ -8,12 +8,29 @@ var divOutput=document.querySelector("#output");
 // divOutput.innerText="Mehul Jain"
 
 // console.log(divOutput);
+                
+var serverURL="https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslatedURL(text)
+{
+    return serverURL+"?"+"text="+text;
+}
+
+function errorHandler(error){
+    console.log("Something wrong with your server, Please try again later");
+    alert("Timeout Error");
+}
+
 
 function clickHandler() {
-    console.log("Clicked");
-    var txt=txtInput.value;
-    divOutput.innerText=txt;
-    console.log(divOutput);
+     console.log("Clicked");
+ fetch(getTranslatedURL(txtInput.value))
+.then(response=>response.json())
+.then(json=> divOutput.innerText=json.contents.translated)
+.catch(errorHandler)
+
+console.log(divOutput);
 }
 
 translateBtn.addEventListener("click", clickHandler);
